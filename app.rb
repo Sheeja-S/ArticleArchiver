f1f3c3a81f09b251f4cb65a898d541685cef4c92
pacelee <pacemlee@gmail.com>
2017-04-18 15:46:21 +0800

    Search

--- app.rb
+++ app.rb
@@ -6,5 +6,23 @@ also_reload("lib/*.rb")
 
 # store side
 get('/') do
+  @articles = Article.all
   erb(:index)
+end
+
+get('/search') do
+  @articles = Article.all
+if params[:search]
+  @articles = Article.search(params[:search])
+else
+  @articles = Article.all
+end
+  erb(:results)
+end
+
+post("/articles") do
+  title = params.fetch("title")
+  article = Article.new({:title => title, :link => "placeholder", :shared_by => "placeholder", :like => 0, :id => nil})
+  article.save()
+  redirect("/")
 end
