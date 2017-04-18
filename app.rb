5e9d7f63d323ebcc4f251c1095c9084d87c286cb
Jeffrey Lee <jeflcc@gmail.com>
2017-04-18 16:17:07 +0800

    routes done

--- app.rb
+++ app.rb
@@ -4,7 +4,42 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 also_reload("lib/*.rb")
 
 
-# store side
+# log in page
 get('/') do
   erb(:index)
+end
+
+# sign up form for log in
+get("/sign_up_form") do
+  erb(:sign_up_form)
+end
+
+# the main page where search and list of all bookmark are shown
+get('/main') do
+  erb(:main)
+end
+
+# route to here after a successful sign up as a new user
+get('/sign_up_form/success') do
+  erb(:success)
+end
+
+#search method
+get('/search') do
+  erb(:main)
+end
+
+#access tag from the nav bar
+get('/tags/:id') do
+  erb(:tag)
+end
+
+#after enter the book mark
+get('/bookmark') do
+  redirect back
+end
+
+# access favorite from the nav bar
+get('/favorites/:id') do
+  erb(:favorite)
 end
