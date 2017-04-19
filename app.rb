3b91b1a567bb024e87f04ae08e34ed3592a6cea4
Richard-NG <richard.chihong.ng@gmail.com>
2017-04-19 16:42:17 +0800

    image added for articles

--- app.rb
+++ app.rb
@@ -70,61 +70,61 @@ end
 
 
 
-  # the main page where search and list of all bookmark are shown
-  get('/main') do
+# the main page where search and list of all bookmark are shown
+get('/main') do
+  @articles = Article.all
+  @tags = Tag.all
+  @user = User.find(session[:user_id])
+  @nav = nav_function (request)
+  erb(:main)
+end
+
+#search method
+get('/search') do
+  @articles = Article.all
+  if params[:search]
+    @articles = Article.search(params[:search])
+  else
     @articles = Article.all
-    @tags = Tag.all
-    @user = User.find(session[:user_id])
-    @nav = nav_function (request)
-    erb(:main)
   end
+  erb(:results)
+end
 
-  #search method
-  get('/search') do
-    @articles = Article.all
-    if params[:search]
-      @articles = Article.search(params[:search])
-    else
-      @articles = Article.all
-    end
-    erb(:results)
-  end
+#access tag from the nav bar
+get('/tags/:id') do
+  erb(:tag)
+end
 
-  #access tag from the nav bar
-  get('/tags/:id') do
-    erb(:tag)
-  end
-
-  #after enter the book mark
-  post('/articles') do
-    @article = Article.create({:title => params.fetch('title'), :link => params.fetch('link'), :shared_by => params.fetch('shared_by')})
-    tag_ids = params.fetch('tag_ids', nil)
-    tag_create = params.fetch('tag_create', nil)
-    if tag_ids != nil and tag_ids.length > 0
-      tag_ids.each do |tag_id|
-        ArticlesTag.create({:article_id => @article.id, :tag_id => tag_id})
-      end
-    end
-    if tag_create != nil
-      @new_tag = Tag.create({:name => tag_create})
-      ArticlesTag.create({:article_id => @article.id, :tag_id => @new_tag.id})
-    end
-
-    if !@article.valid? or !@new_tag.valid?
-      erb(:add_article_error)
-    else
-      redirect to ('/main')
+#after enter the book mark
+post('/articles') do
+  @article = Article.create({:title => params.fetch('title'), :link => params.fetch('link'), :image => params.fetch('image'),:shared_by => params.fetch('shared_by')})
+  tag_ids = params.fetch('tag_ids', nil)
+  tag_create = params.fetch('tag_create', nil)
+  if tag_ids != nil and tag_ids.length > 0
+    tag_ids.each do |tag_id|
+      ArticlesTag.create({:article_id => @article.id, :tag_id => tag_id})
     end
   end
-
-  # access favorite from the nav bar
-  get('/favorites/:id') do
-    erb(:favorite)
+  if tag_create != nil
+    @new_tag = Tag.create({:name => tag_create})
+    ArticlesTag.create({:article_id => @article.id, :tag_id => @new_tag.id})
   end
 
-  post("/articles") do
-    title = params.fetch("title")
-    article = Article.new({:title => title, :link => "placeholder", :shared_by => "placeholder", :like => 0, :id => nil})
-    article.save()
-    redirect("/")
+  if !@article.valid? or !@new_tag.valid?
+    erb(:add_article_error)
+  else
+    redirect to ('/main')
   end
+end
+
+# access favorite from the nav bar
+get('/favorites/:id') do
+  erb(:favorite)
+end
+
+post("/articles") do
+  title = params.fetch("title")
+  article = Article.new({:title => title, :link => "placeholder", :shared_by => "placeholder", :like => 0, :id => nil})
+  article.save()
+  redirect("/")
+end
